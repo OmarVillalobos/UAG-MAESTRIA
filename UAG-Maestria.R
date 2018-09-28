7426244c372b1a526ec1b263ce7b7edd96c1a535
OmarVillalobos <omar_vr@live.com.mx>
2018-09-28 18:26:06 -0500

    Add files via upload

--- UAG-Maestria.R
+++ UAG-Maestria.R
@@ -1,18 +1,22 @@
 # https://www.kaggle.com/blastchar/telco-customer-churn/home
-
+rm() # remueve la asignacion de una variable
 
 datos <- read.csv("C:/Users/OmarVr/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv")
-summary(datos)
+summary(datos) # sumario estadistico de un data frame
 
-library(ggplot2)
-library(dplyr)
+library(ggplot2) # libreria de graficos
+library(dplyr) # libreria de manejo de datos
 
 ggplot(datos, aes(x=Churn, y = gender)) + 
   geom_jitter()
 
-  datos.payment <- datos %>%
-    filter(PaymentMethod != "Electronic check") %>%
-    filter(PaymentMethod != "Mailed check")
-  ggplot(datos.payment , aes(x = Churn, y = gender, colour = PaymentMethod)) + 
+datos.payment <- datos %>%
+  filter(PaymentMethod != "Electronic check") %>%
+  filter(PaymentMethod != "Mailed check")
+ggplot(datos.payment , aes(x = Churn, y = gender, colour = PaymentMethod)) + 
+  geom_jitter()
+
+
+ggplot(datos, aes(x = Churn, y = SeniorCitizen, size = tenure)) +
   geom_jitter()
   
